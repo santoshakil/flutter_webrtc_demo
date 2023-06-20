0b114aefd4b36c75a9762cea7b5e058a3fd6eca1
Santo Shakil <shakil.mezbah@gmail.com>
2023-06-20 13:56:20 +0600

    riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
